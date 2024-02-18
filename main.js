b551ab704aeec8652ab1776f38e3d994ead32ff5
jluisferrer <jluisferrer@gmail.com>
2024-02-18 12:30:00 +0100

    Funcional a prueba de errores

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-export const fn = (/* argumentos de la función */) => {
-   // Cuerpo de la función: Completa la lógica de la función aquí.
+export const fn = (a, b, c) => {
+   if (a < 0) {
+       throw Error("El valor de 'a' no puede ser negativo.");
+   } else if (b < 0) {
+       throw Error("El valor de 'b' no puede ser negativo.");
+   } else if (c < 0) {
+       throw Error("El valor de 'c' no puede ser negativo.");
+   } else if (a + b + c >= 40) {
+       throw Error("La cantidad total de empanadas no puede ser superior a 40");
+   } else if ((a + b + c) % 3 !== 0) {
+       throw Error("La suma de 'a', 'b' y 'c' debe ser divisible por 3.");
+   } else {
+       return Math.ceil((a * 12 + b * 14 + c * 16) / 3);
+   }
 };
